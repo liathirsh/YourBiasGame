94abe712a3931b0a015635638d253c6f4ccc65cc
Liat Hirsh <liat@urbanpixels.com>
2024-10-05 18:46:06 -0400

    Fix results page CSS issue

--- src/app/(endgame)/results/page.tsx
+++ src/app/(endgame)/results/page.tsx
@@ -19,12 +19,17 @@ const ResultsPage = () => {
 
     return (
         <div className="h-screen flex flex-col items-center justify-center">
-          <h1 className="text-3xl font-bold mb-6">Game Results</h1>
-          <p className="text-lg mb-4">
+          <h1 className="text-3xl font-bold mb-6 text-gray-200">Game Results</h1>
+          <p className="text-lg mb-4 text-gray-200">
             You scored <strong>{score}</strong> out of <strong>{total}</strong>!
           </p>
-          <p className="text-lg mb-8">That&apos;s a <strong>{percentage}%</strong> success rate.</p>
-          
+          <p className="text-lg mb-8 text-gray-200">That&apos;s a <strong>{percentage}%</strong> success rate.</p>
+          {score === 4 ? (
+            <p className="text-lg mb-8 text-gray-200">You got a perfect score! You know your biases!</p>
+          ) : (
+            <p className="text-lg mb-8 text-gray-200"> Play again to learn about those biases!</p>  
+          )}
+
           {/* <div className="w-full max-w-md bg-gray-950 p-6 rounded-lg shadow-md">
             <h2 className="text-xl font-bold mb-4">How other players scored:</h2>
             <table className="w-full border-collapse">
